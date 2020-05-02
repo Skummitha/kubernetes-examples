ef35a8ba295b47689efeb426600b922f494dac84
KalluruS <>
2020-05-02 22:04:09 +0530

    Adding files

--- cluster-setups/aws/eks-with-node-groups/eks.tf
+++ cluster-setups/aws/eks-with-node-groups/eks.tf
@@ -1,8 +1,8 @@
-module "EKS" {
-  source          = "github.com/linuxautomations/terraform-modules.git//eks-node-groups"
-  PURPOSE         = "sample"
-  PUBLIC_SUBNETS  = module.VPC.PUBLIC_SUBNETS
-  KEYPAIR_NAME    = "sriharsha"
-  INSTANCE_TYPE   = "c5.large"
-  INSTANCE_COUNT  = 2
-}
+//module "EKS" {
+//  source          = "github.com/linuxautomations/terraform-modules.git//eks-node-groups"
+//  PURPOSE         = "sample"
+//  PUBLIC_SUBNETS  = module.VPC.PUBLIC_SUBNETS
+//  KEYPAIR_NAME    = "sriharsha"
+//  INSTANCE_TYPE   = "c5.large"
+//  INSTANCE_COUNT  = 2
+//}
