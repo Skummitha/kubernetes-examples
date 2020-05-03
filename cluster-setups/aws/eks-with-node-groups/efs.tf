31b39b5c01857999977b45d26c3bdcb314067c3f
KalluruS <>
2020-05-03 08:48:13 +0530

    Adding files

--- cluster-setups/aws/eks-with-node-groups/efs.tf
+++ cluster-setups/aws/eks-with-node-groups/efs.tf
@@ -5,3 +5,9 @@ resource "aws_efs_file_system" "shared-volume-eks" {
     Name = local.EFS_NAME
   }
 }
+
+resource "aws_efs_mount_target" "public" {
+  count           = length(module.VPC.PUBLIC_SUBNETS)
+  file_system_id  = aws_efs_file_system.shared-volume-eks.id
+  subnet_id       = element(module.VPC.PUBLIC_SUBNETS, count.index)
+}
