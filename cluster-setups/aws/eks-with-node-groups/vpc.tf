d549922534391a00b049cdfe573117d201219ac3
KalluruS <>
2020-05-03 08:44:39 +0530

    Adding files

--- cluster-setups/aws/eks-with-node-groups/vpc.tf
+++ cluster-setups/aws/eks-with-node-groups/vpc.tf
@@ -1,5 +1,5 @@
 module "VPC" {
   source        = "github.com/linuxautomations/terraform-modules.git//vpc"
   VPC_CIDR      = "10.233.0.0/16"
-  PURPOSE       = var.VPC
+  PURPOSE       = var.PURPOSE
 }
