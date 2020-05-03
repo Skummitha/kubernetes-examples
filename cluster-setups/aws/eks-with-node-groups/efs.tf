aab47e74bbb5b93aeb2ef456ff0e67d4d21f0509
KalluruS <>
2020-05-03 09:25:34 +0530

    Adding files

--- cluster-setups/aws/eks-with-node-groups/efs.tf
+++ cluster-setups/aws/eks-with-node-groups/efs.tf
@@ -48,7 +48,7 @@ resource "null_resource" "mount_efs" {
 
     inline = [
       "sudo yum install -y amazon-efs-utils",
-      "sudo mkdir -p /data"
+      "sudo mkdir -p /data",
       "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_file_system.shared-volume-eks.dns_name}:/ /data"
     ]
 
