93ee84a51eced355194abcb1b1f8a4edf5581341
KalluruS <>
2020-05-03 08:29:41 +0530

    Adding files

--- cluster-setups/aws/eks-with-node-groups/outputs.tf
+++ cluster-setups/aws/eks-with-node-groups/outputs.tf
@@ -1,3 +1,3 @@
 output "AUTO_SCALING_GROUP_NAME" {
-  value = tolist(module.EKS.AUTO_SCALING_GROUP_NAME.*.autoscaling_groups[*])
+  value = join(module.EKS.AUTO_SCALING_GROUP_NAME.*.autoscaling_groups[*])
 }
